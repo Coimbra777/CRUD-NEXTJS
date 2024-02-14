411a2ed743b7abfe8d20a159a11f75280a25aba9
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2024-02-14 05:18:44 -0300

    components titulo e layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Layout from "@/components/Layout";
+
 export default function Home() {
   return (
-    <div className={`flex justify-center items-center h-screen`}>
-      <h1 className="text-3xl">App</h1>
+    <div className="flex justify-center items-center h-screen">
+      <Layout titulo="Cadastro Simples">
+        <span>conteudo</span>
+      </Layout>
     </div>
   );
 }
