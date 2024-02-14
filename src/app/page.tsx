cb32a0e80124ebb6815ac97ebdad5ed773ac0a5f
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2024-02-14 05:51:03 -0300

    criação tabela e dados de clientes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 export default function Home() {
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Pedro", 36, "2"),
+    new Cliente("Carlos", 24, "3"),
+    new Cliente("João", 40, "4"),
+  ];
   return (
     <div className="flex justify-center items-center h-screen">
       <Layout titulo="Cadastro Simples">
-        <span>conteudo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   );
